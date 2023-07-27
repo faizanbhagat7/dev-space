fadee6945f53fe66bf9ba205d1e2004eb3f983d1
Faizan <faizanfaruk8@gmail.com>
2023-07-27 11:15:05 +0530

    followers module page added bugless

--- src/components/profile/FollowersPage.jsx
+++ src/components/profile/FollowersPage.jsx
@@ -1,56 +1,79 @@
-import React,{useState,useEffect,useContext} from 'react'
-import './FollowersPage.css'
-import { LoginContext } from '../../context/LoginContext'
-import { supabase } from '../../backend/supabaseConfig'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect, useContext } from "react";
+import "./FollowersPage.css";
+import { LoginContext } from "../../context/LoginContext";
+import { supabase } from "../../backend/supabaseConfig";
+import { Link,useParams } from "react-router-dom";
+
 
 const FollowersPage = () => {
-    const {user} = useContext(LoginContext)
-    const [followers,setFollowers] = useState([])
+  const {profileId} = useParams();
+  const [followers, setFollowers] = useState([]);
 
-    const fetchFollowerProfiles = async () => {
-        const {data,error} = await supabase
-        .from('profiles')
-        .select()
-        .in('id',user?.followers)
-        if(error){
-            console.log(error)
-            return
-        }
-        setFollowers(data)
+  const fetchFollowerProfiles = async () => {
+
+    // fetch followers id 
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("followers")
+      .eq("id", profileId)
+  
+    if (error) {
+      console.log(error);
+      return;
+    }
+    // fetch followers profile
+    let idList = data[0]?.followers;
+    console.log(idList);
+
+    if (idList === null || idList === []) {
+      return;
+    }
+    else{
+      const { data, error } = await supabase
+      .from("profiles")
+      .select()
+      .in("id", idList)
+      setFollowers(data);
+      if (error) {
+        console.log(error);
+        return;
+      }
     }
 
-    useEffect(() => {
-        fetchFollowerProfiles()
-    }, [])
+  };
 
-    return (
+  useEffect(() => {
+    fetchFollowerProfiles();
+  }, []);
+
+  return (
     <>
-        <div className='followers-container'>
-    {
-        user?.followers?.length === 0 ? <h1>No followers yet</h1> : <h1>Followers</h1>
-    }
-    
-         {followers.length > 0 &&
-            followers?.map((fetcheduser) => (
-              <Link
-                to={`/profile/${fetcheduser.id}`}
-                style={{ textDecoration: "none", color: "black" }}
-              >
-                <div className="follower-card">
-                  <div className="follower-image">
-                    <img src={fetcheduser.avatar} alt="" />
-                  </div>
-                  <div className="follower-details">
-                    <div className="follower-name">{fetcheduser.name}</div>
-                  </div>
+      <div className="followers-container">
+        {followers?.length === 0 ? (
+          <h1>No followers yet</h1>
+        ) : (
+          <h1>Followers</h1>
+        )}
+
+        {followers.length > 0 &&
+          followers?.map((fetcheduser) => (
+            <Link
+              to={`/profile/${fetcheduser.id}`}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <div className="follower-card">
+                <div className="follower-image">
+                  <img src={fetcheduser.avatar} alt="" />
                 </div>
-              </Link>
-            ))}
-
-</div>
+                <div className="follower-details">
+                  <div className="follower-name">{fetcheduser.name}</div>
+                </div>
+              </div>
+            </Link>
+          ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default FollowersPage
+export default FollowersPage;
